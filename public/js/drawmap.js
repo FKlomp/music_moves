86c8cc983add7dbb8764844c99afd3c82d088366
Ferdi Klomp <ferdi.klomp@24imedia.com>
2016-03-14 21:08:00 +0100

    - Added lines on map

--- public/js/drawmap.js
+++ public/js/drawmap.js
@@ -1,75 +1,134 @@
 var format = function(d) {
-			    d = d;
-			    return d3.format(',.00f')(d);
-			}
+    d = d;
+    return d3.format(',.00f')(d);
+}
 
-			//domein van kleurtjes
-			var domainArray = [1,100];
+var width = 1152,
+    height = 600;
 
+var domainArray = [1, 100];
+    
+var map = d3.geomap.choropleth()
+    .geofile('js/vendor/d3-geomap/topojson/world/countries.json')
+    .projection(d3.geo.naturalEarth)
+    .width(width)
+    .height(height)
+    .scale(200)
+    .translate([width / 2, height / 2])
+    .zoomFactor(2)
+    .colors(colorbrewer.Reds[9])
+    .column('2014-04-01T00:00:00.000Z') //nog animeren
+    .domain(domainArray)
+    .format(format)
+    .legend(true, {transform: "translate(0,320)"})
+    .unitId('Country Code');
 
-			var map = d3.geomap.choropleth()
-			    .geofile('js/vendor/d3-geomap/topojson/world/countries.json')
-			    .colors(colorbrewer.Reds[9])
-			    .column('2014-04-01T00:00:00.000Z') //nog animeren
-				.domain(domainArray)
-			    .format(format)
-			    .legend(true)
-			    .unitId('Country Code');
+var projection = d3.geo.naturalEarth().scale(map.properties.scale).translate(map.properties.translate).precision(0.1);
 
-			d3.csv('/data/streamwatchr_dump.csv', function(error, data) {
-				d3.csv('/data/country_codes.csv', function(error2, data2) {
-					var totalData = getData(data,data2);
-					console.log(totalData);
-					d3.select('#map')
-					.datum(totalData)
-					.call(map.draw, map);
+var path = d3.geo.path()
+    .projection(projection);
 
+d3.csv('/data/streamwatchr_dump.csv', function(error, data) {
+    d3.csv('/data/country_codes.csv', function(error2, data2) {
+        var totalData = getData(data, data2);
+        console.log(totalData);
+        d3.select('#map')
+            .datum(totalData)
+            .call(map.draw, map);
+        
+        setTimeout(function () {
+            d3.select('g.zoom')
+                .append('g')
+                .selectAll("path .edge") 
+                .data([{ 
+                    "type": "LineString",
+                    "coordinates": [
+                        [4.8909347, 52.3738007], [-60.8909347, 20.3738007]
+                    ]
+                },{ 
+                    "type": "LineString",
+                    "coordinates": [
+                        [4.8909347, 52.3738007], [37.618423, 55.751244]
+                    ]
+                },{ 
+                    "type": "LineString",
+                    "coordinates": [
+                        [4.8909347, 52.3738007], [42.8909347, 57.3738007]
+                    ]
+                },{ 
+                    "type": "LineString",
+                    "coordinates": [
+                        [4.8909347, 52.3738007], [10.8909347, 15.3738007]
+                    ]
+                },{ 
+                    "type": "LineString",
+                    "coordinates": [
+                        [4.8909347, 52.3738007], [20.8909347, 30.3738007]
+                    ]
+                },{ 
+                    "type": "LineString",
+                    "coordinates": [
+                        [4.8909347, 52.3738007], [1.8909347, 3.3738007]
+                    ]
+                },{ 
+                    "type": "LineString",
+                    "coordinates": [
+                        [4.8909347, 52.3738007], [40.8909347, 10.3738007]
+                    ]
+                },{ 
+                    "type": "LineString",
+                    "coordinates": [
+                        [4.8909347, 52.3738007], [60.8909347, 20.3738007]
+                    ]
+                }])
+                .enter()
+                .append("path")
+                .attr("class", "edge")
+                .attr("d", path)
+                .attr("stroke-dasharray", function() { return this.getTotalLength() + " " + this.getTotalLength(); } )
+                .attr("stroke-dashoffset", function() { return this.getTotalLength(); } )
+                .transition()
+                .duration(2000)
+                .ease("linear")
+                .attr("stroke-dashoffset", 0);
+        }, 1000);
+    });
+});
 
+function getData(data, data2) {
+    //maak object per land
+    var totalData = [];
+    var iso2Code = 'Country Code2';
+    for (var i = 0; i < data2.length; i++) {
+        var country = {
+            'Country Code2': data2[i].Alpha_2,
+            'Country Code': data2[i].Alpha_3
+        };
+        totalData.push(country);
+    }
 
+    //count bij bijbehorende tijd, als bijbehorend tijd er niet is maak die aan en count
+    for (var i = 0; i < data.length; i++) {
+        var country = data[i].country;
+        var date = data[i].date;
+        var place = findObject(totalData, iso2Code, country);
+        if (place && data[i].count) {
+            if (totalData[place][date]) {
+                totalData[place][date] += parseInt(data[i].count);
+            } else {
 
-					function getData(data,data2)
-					{
-						//maak object per land
-						var totalData = [];
-						var iso2Code = 'Country Code2';
-						for(var i = 0; i < data2.length; i++) 
-						{
-							var country = {'Country Code2' : data2[i].Alpha_2, 'Country Code' : data2[i].Alpha_3};
-							totalData.push(country);
-						}
+                totalData[place][date] = parseInt(data[i].count);
+            }
+        }
+    }
+    return totalData;
+}
 
-						//count bij bijbehorende tijd, als bijbehorend tijd er niet is maak die aan en count
-						for(var i = 0; i < data.length; i++)
-						{
-							var country = data[i].country;
-							var date = data[i].date;
-							var place = findObject(totalData,iso2Code,country);
-							if(place && data[i].count)
-							{
-								if (totalData[place][date]) 
-								{
-									totalData[place][date] += parseInt(data[i].count);
-								}
-								else
-								{
-									
-									totalData[place][date] = parseInt(data[i].count);
-								}
-							}
-						}
-						return totalData;
-					}
+function findObject(objectArray, property, name) {
+    for (var i = 0; i < objectArray.length; i++) {
+        if (objectArray[i][property] == name) {
+            return (i);
+        }
+    }
 
-					function findObject(objectArray,property,name)
-					{
-						for(var i = 0; i < objectArray.length; i++)
-						{
-							if (objectArray[i][property] == name)
-							{
-								return(i);
-							}
-						}
-
-					}
-				});
-			});
+}
--- public/js/search_bar.js
+++ public/js/search_bar.js
@@ -1,19 +1,82 @@
 $( document ).ready(function() {
-    $('#search-bar').change(function () {
-        $.get('api/streamwatch/song', {q: $(this).val()})
-        .done(function( data ) {
-            var resultDiv = $('#search-results')
-            resultDiv.empty('');
-            
-            for (var i = 0; i < data.length; i++) {
-                var row = '<tr>' +
-                    '<td>' + data[i].artists[0].name + '</td>' +
-					'<td>' + data[i].song + '</td>' +
-					'<td>N/A</td>' +
-				'</tr>';
-                
-                resultDiv.append(row); 
+    var selectedQueryType = 0,
+        queryTypes = [{
+            name: 'Song',
+            url: 'api/streamwatch/song'
+        },{
+            name: 'Artist',
+            url: 'api/streamwatch/artist'
+        }];
+    
+    $('#filter-button').html(queryTypes[selectedQueryType].name + ' <span class="caret"></span>');
+    
+    var buildDropdown = function () {
+        $('#filter-dropdown').empty();
+
+        for (var i = 0; i < queryTypes.length; i++) {
+            if(i != selectedQueryType) {
+                $('<li><a id="query-' + i + '" class="filter-option" href="#">' + queryTypes[i].name + '</a></li>')
+                .appendTo('#filter-dropdown');
             }
+        }
+    }
+    buildDropdown();
+    
+    $(document).on('click', '.filter-option', function (evt) {
+        selectedQueryType = evt.target.id.split('-')[1];
+        $('#filter-button').html(queryTypes[selectedQueryType].name + ' <span class="caret"></span>');
+        
+        buildDropdown();
+    });
+    
+    $('#search-bar').focus(function () {
+        $(this).val('');
+    });
+    
+    $('#search-results').on('click', '.list-group-item', function (evt) {
+        console.log(evt.target.id) 
+    });
+    
+    $('#search-submit').click(function () {
+        $.get(queryTypes[selectedQueryType].url, {q: $('#search-bar').val()})
+        .done(function( data ) {
+            var i,
+                row,
+                resultDiv = $('#search-results');
+            
+            resultDiv.empty();
+            
+            for (i = 0; i < data.length; i++) {
+                if (selectedQueryType == 0) {
+                    $('<button id="' + data[i].artistMbId + '" type="button" class="list-group-item">' +
+                        data[i].artists[0].name + ' - ' + data[i].song +
+                        '</button>')
+                    .hide()
+                    .appendTo('#search-results')
+                    .slideDown(500);
+                } else {
+                    $('<button id="' + data[i].mbId + '" type="button" class="list-group-item">' +
+                        data[i].lastfm_info.artist.name +
+                        '</button>')
+                    .hide()
+                    .appendTo('#search-results')
+                    .slideDown(500);
+                }
+            }
+            
+            var removeResults = function (evt) {
+                if (!evt.target.classList.contains('list-group-item')) {
+                    evt.preventDefault();
+                    evt.stopPropagation();
+                    
+                    $('#search-results .list-group-item')
+                    .slideUp(500);
+                    
+                    document.removeEventListener('click', removeResults, true);
+                }
+            };
+            
+            document.addEventListener('click', removeResults, true); 
         });
     })
 });
